fe69360fa71200b2e86403a5967c6fc131e53853
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-01-12 18:43:53 -0600

    parameterize number of rounds through TEA via a compile-time template param

--- common/simd.h
+++ common/simd.h
@@ -237,25 +237,25 @@ namespace ospray {
     ///////////////////////////////////////////////////////////////////////////
     // TEA - Random numbers based on Tiny Encryption Algorithm
 
-    template <typename T>
+    template <typename T, int NUM_ROUNDS = 8>
     inline void tea8(T& v0, T& v1)
     {
       T sum{0};
 
-      for(int i = 0; i < 8; i++) { // just 8 instead of 32 rounds
+      for(int i = 0; i < NUM_ROUNDS; i++) {
         sum += 0x9e3779b9;
         v0 += ((v1 << 4) + 0xa341316c) ^ (v1 + sum) ^ ((v1 >> 5) + 0xc8013ea4);
         v1 += ((v0 << 4) + 0xad90777d) ^ (v0 + sum) ^ ((v0 >> 5) + 0x7e95761e);
       }
     }
 
-    template <typename T>
+    template <typename T, int NUM_TEA_ROUNDS = 8>
     struct RandomTEA
     {
       RandomTEA(const T &idx, const T &seed) : v0(idx), v1(seed) {}
       simd::vec2f getFloats()
       {
-        tea8(v0, v1);
+        tea8<T, NUM_TEA_ROUNDS>(v0, v1);
         const float tofloat = 2.3283064365386962890625e-10f; // 1/2^32
         return {simd::cast<vfloat>(v0) * tofloat,
                 simd::cast<vfloat>(v1) * tofloat};
