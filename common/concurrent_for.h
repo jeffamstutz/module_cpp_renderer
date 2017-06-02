b51db4c5d0c13640768ab8d212cc1fc7454ee55c
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-06-02 14:30:40 -0500

    use method where fiber lifetimes are maintained...artifacts + bad perf....

--- common/concurrent_for.h
+++ common/concurrent_for.h
@@ -19,12 +19,59 @@
 #include "ospcommon/TypeTraits.h"
 
 #include <boost/fiber/all.hpp>
+#include <functional>
 
 namespace ospray {
   namespace cpp_renderer {
+    namespace detail {
+      extern thread_local std::function<void(int)> currentFiberTask;
+      extern thread_local bool newTask;
+      extern thread_local bool cancelFibers;
+      extern thread_local int numFinishedFibers;
 
-    // NOTE(jda) - This abstraction wraps "fork-join" parallelism, with an
-    //             implied synchronizsation after all of the tasks have run.
+      struct FiberGroup
+      {
+        inline ~FiberGroup() { clearFibers(); }
+
+        inline void clearFibers()
+        {
+          if (!activeFibers.empty()) {
+            cancelFibers = true;
+            newTask = false;
+
+            for (auto &f : activeFibers)
+              f.join();
+
+            activeFibers.clear();
+          }
+        }
+
+        std::vector<boost::fibers::fiber> activeFibers;
+      };
+
+      extern thread_local FiberGroup fibers;
+
+      static void fiber_fcn(int whichFiber, int numSiblingFibers)
+      {
+        while (true) {
+          if (cancelFibers)
+            return;
+
+          if (!newTask) {
+            boost::this_fiber::yield();
+            continue;
+          }
+
+          currentFiberTask(whichFiber);
+
+          numFinishedFibers++;
+
+          if (numFinishedFibers == numSiblingFibers)
+            newTask = false;
+        }
+      }
+    } // ::ospray::cpp_renderer::detail
+
     template<typename FCN_T>
     inline void concurrent_for(int nFibers, FCN_T&& fcn)
     {
@@ -33,13 +80,24 @@ namespace ospray {
                     "ospray::cpp_renderer::concurrent_for() requires FCN_T"
                     " to implement operator() with a single 'int' parameter.");
 
-      std::vector<boost::fibers::fiber> fibers;
+      if (detail::fibers.activeFibers.size() != size_t(nFibers)) {
+        detail::fibers.activeFibers.resize(nFibers);
 
-      for (int i = 0; i < nFibers; ++i)
-        fibers.emplace_back(fcn, i);
+        int whichFiber = 0;
+        for (auto &fiber : detail::fibers.activeFibers)
+          fiber = boost::fibers::fiber(boost::fibers::launch::post,
+                                       detail::fiber_fcn,
+                                       whichFiber++,
+                                       nFibers);
+      }
 
-      for (auto &f : fibers)
-        f.join();
+      detail::numFinishedFibers = 0;
+
+      detail::currentFiberTask = fcn;
+      detail::newTask = true;
+
+      while (detail::numFinishedFibers < nFibers)
+        boost::this_fiber::yield();
     }
 
   } // ::ospray::cpp_renderer
