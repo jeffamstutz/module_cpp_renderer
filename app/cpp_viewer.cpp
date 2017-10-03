1b2f9d526ad482e9c2ef21231ce0fe69150a09d4
Jefferson Amstutz <jeffamstutz@gmail.com>
2017-10-03 13:28:42 -0500

    fix indentation

--- app/cpp_viewer.cpp
+++ app/cpp_viewer.cpp
@@ -28,220 +28,220 @@ namespace ospray {
 
     using namespace sg;
 
-static std::vector<ospcommon::FileName> files;
+    static std::vector<ospcommon::FileName> files;
 
-//parse command line arguments containing the format:
-//  -nodeName:...:nodeName=value,value,value -- changes value
-//  -nodeName:...:nodeName+=name,type        -- adds new child node
-static inline void parseCommandLineSG(int ac, const char **&av, sg::Node &root)
-{
-  for(int i=1;i < ac; i++) {
-    std::string arg(av[i]);
-    size_t f;
-    std::string value("");
-    if (arg.size() < 2 || arg[0] != '-')
-      continue;
-
-    const std::string orgarg(arg);
-    while ((f = arg.find(":")) != std::string::npos ||
-           (f = arg.find(",")) != std::string::npos) {
-      arg[f] = ' ';
-    }
-
-    f = arg.find("+=");
-    bool addNode = false;
-    if (f != std::string::npos)
+    //parse command line arguments containing the format:
+    //  -nodeName:...:nodeName=value,value,value -- changes value
+    //  -nodeName:...:nodeName+=name,type        -- adds new child node
+    static inline void parseCommandLineSG(int ac, const char **&av,
+                                          sg::Node &root)
     {
-      value = arg.substr(f+2,arg.size());
-      addNode = true;
-    }
-    else
-    {
-      f = arg.find("=");
-      if (f != std::string::npos)
-        value = arg.substr(f+1,arg.size());
-    }
+      for(int i=1;i < ac; i++) {
+        std::string arg(av[i]);
+        size_t f;
+        std::string value("");
+        if (arg.size() < 2 || arg[0] != '-')
+          continue;
 
-    if (value != "") {
-      std::stringstream ss;
-      ss << arg.substr(1,f-1);
-      std::string child;
-      std::reference_wrapper<sg::Node> node_ref = root;
-      try {
-        while (ss >> child) {
-          node_ref = node_ref.get().childRecursive(child);
+        const std::string orgarg(arg);
+        while ((f = arg.find(":")) != std::string::npos ||
+               (f = arg.find(",")) != std::string::npos) {
+          arg[f] = ' ';
         }
-      } catch (const std::runtime_error &) {
-        std::cerr << "Warning: unknown sg::Node '" << child
-          << "', ignoring option '" << orgarg << "'." << std::endl;
-      }
-      auto &node = node_ref.get();
 
-      std::stringstream vals(value);
-
-      if (addNode) {
-        std::string name, type;
-        vals >> name >> type;
-        try {
-          node.createChild(name, type);
-        } catch (const std::runtime_error &) {
-          std::cerr << "Warning: unknown sg::Node type '" << type
-            << "', ignoring option '" << orgarg << "'." << std::endl;
+        f = arg.find("+=");
+        bool addNode = false;
+        if (f != std::string::npos)
+        {
+          value = arg.substr(f+2,arg.size());
+          addNode = true;
         }
-      } else { // set node value
+        else
+        {
+          f = arg.find("=");
+          if (f != std::string::npos)
+            value = arg.substr(f+1,arg.size());
+        }
 
-        // TODO: more generic implementation
-        if (node.valueIsType<std::string>()) {
-          node.setValue(value);
-        } else if (node.valueIsType<float>()) {
-          float x;
-          vals >> x;
-          node.setValue(x);
-        } else if (node.valueIsType<int>()) {
-          int x;
-          vals >> x;
-          node.setValue(x);
-        } else if (node.valueIsType<bool>()) {
-          bool x;
-          vals >> x;
-          node.setValue(x);
-        } else if (node.valueIsType<ospcommon::vec3f>()) {
-          float x,y,z;
-          vals >> x >> y >> z;
-          node.setValue(ospcommon::vec3f(x,y,z));
-        } else if (node.valueIsType<ospcommon::vec2i>()) {
-          int x,y;
-          vals >> x >> y;
-          node.setValue(ospcommon::vec2i(x,y));
-        } else try {
-          auto &vec = dynamic_cast<sg::DataVector1f&>(node);
-          float f;
-          while (vals.good()) {
-            vals >> f;
-            vec.push_back(f);
+        if (value != "") {
+          std::stringstream ss;
+          ss << arg.substr(1,f-1);
+          std::string child;
+          std::reference_wrapper<sg::Node> node_ref = root;
+          try {
+            while (ss >> child) {
+              node_ref = node_ref.get().childRecursive(child);
+            }
+          } catch (const std::runtime_error &) {
+            std::cerr << "Warning: unknown sg::Node '" << child
+              << "', ignoring option '" << orgarg << "'." << std::endl;
           }
-        } catch(...) {}
+          auto &node = node_ref.get();
+
+          std::stringstream vals(value);
+
+          if (addNode) {
+            std::string name, type;
+            vals >> name >> type;
+            try {
+              node.createChild(name, type);
+            } catch (const std::runtime_error &) {
+              std::cerr << "Warning: unknown sg::Node type '" << type
+                << "', ignoring option '" << orgarg << "'." << std::endl;
+            }
+          } else { // set node value
+
+            // TODO: more generic implementation
+            if (node.valueIsType<std::string>()) {
+              node.setValue(value);
+            } else if (node.valueIsType<float>()) {
+              float x;
+              vals >> x;
+              node.setValue(x);
+            } else if (node.valueIsType<int>()) {
+              int x;
+              vals >> x;
+              node.setValue(x);
+            } else if (node.valueIsType<bool>()) {
+              bool x;
+              vals >> x;
+              node.setValue(x);
+            } else if (node.valueIsType<ospcommon::vec3f>()) {
+              float x,y,z;
+              vals >> x >> y >> z;
+              node.setValue(ospcommon::vec3f(x,y,z));
+            } else if (node.valueIsType<ospcommon::vec2i>()) {
+              int x,y;
+              vals >> x >> y;
+              node.setValue(ospcommon::vec2i(x,y));
+            } else try {
+              auto &vec = dynamic_cast<sg::DataVector1f&>(node);
+              float f;
+              while (vals.good()) {
+                vals >> f;
+                vec.push_back(f);
+              }
+            } catch(...) {}
+          }
+        }
       }
     }
-  }
-}
 
-void parseExtraParametersFromComandLine(int ac, const char **&av)
-{
-  bool isStream = false;
-  bool isSimd   = false;
+    void parseExtraParametersFromComandLine(int ac, const char **&av)
+    {
+      bool isStream = false;
+      bool isSimd   = false;
 
-  for (int i = 1; i < ac; i++) {
-    const std::string arg = av[i];
-    if (arg == "--simd" || arg == "-simd") {
-      isSimd = true;
-    } else if (arg == "--stream" || arg == "-stream") {
-      isStream = true;
-    } else {
-      ospcommon::FileName fileName = arg;
-      if (fileName.ext() == "obj")
-        files.push_back(arg);
-      else
-        std::cout << "Ignoring non-obj file " << arg << std::endl;
+      for (int i = 1; i < ac; i++) {
+        const std::string arg = av[i];
+        if (arg == "--simd" || arg == "-simd") {
+          isSimd = true;
+        } else if (arg == "--stream" || arg == "-stream") {
+          isStream = true;
+        } else {
+          ospcommon::FileName fileName = arg;
+          if (fileName.ext() == "obj")
+            files.push_back(arg);
+          else
+            std::cout << "Ignoring non-obj file " << arg << std::endl;
+        }
+      }
+
+      if (isStream) g_postfix += "_stream";
+      if (isSimd)   g_postfix += "_simd";
     }
-  }
 
-  if (isStream) g_postfix += "_stream";
-  if (isSimd)   g_postfix += "_simd";
-}
+    static inline void addLightsToScene(sg::Node& renderer)
+    {
+      auto &lights = renderer["lights"];
 
-static inline void addLightsToScene(sg::Node& renderer)
-{
-  auto &lights = renderer["lights"];
+    #if 0
+      auto &sun = lights.createChild("sun", "DirectionalLight");
+      sun["color"] = vec3f(1.f,232.f/255.f,166.f/255.f);
+      sun["direction"] = vec3f(0.462f,-1.f,-.1f);
+      sun["intensity"] = 1.5f;
 
-#if 0
-  auto &sun = lights.createChild("sun", "DirectionalLight");
-  sun["color"] = vec3f(1.f,232.f/255.f,166.f/255.f);
-  sun["direction"] = vec3f(0.462f,-1.f,-.1f);
-  sun["intensity"] = 1.5f;
+      auto &bounce = lights.createChild("bounce", "DirectionalLight");
+      bounce["color"] = vec3f(127.f/255.f,178.f/255.f,255.f/255.f);
+      bounce["direction"] = vec3f(-.93,-.54f,-.605f);
+      bounce["intensity"] = 0.25f;
 
-  auto &bounce = lights.createChild("bounce", "DirectionalLight");
-  bounce["color"] = vec3f(127.f/255.f,178.f/255.f,255.f/255.f);
-  bounce["direction"] = vec3f(-.93,-.54f,-.605f);
-  bounce["intensity"] = 0.25f;
-
-  auto &ambient = lights.createChild("ambient", "AmbientLight");
-  ambient["intensity"] = 0.9f;
-  ambient["color"] = vec3f(174.f/255.f,218.f/255.f,255.f/255.f);
-#endif
-}
+      auto &ambient = lights.createChild("ambient", "AmbientLight");
+      ambient["intensity"] = 0.9f;
+      ambient["color"] = vec3f(174.f/255.f,218.f/255.f,255.f/255.f);
+    #endif
+    }
 
 
-extern "C" int main(int ac, const char **av)
-{
-  int init_error = ospInit(&ac, av);
-  if (init_error != OSP_NO_ERROR) {
-    std::cerr << "FATAL ERROR DURING INITIALIZATION!" << std::endl;
-    return init_error;
-  }
+    extern "C" int main(int ac, const char **av)
+    {
+      int init_error = ospInit(&ac, av);
+      if (init_error != OSP_NO_ERROR) {
+        std::cerr << "FATAL ERROR DURING INITIALIZATION!" << std::endl;
+        return init_error;
+      }
 
-  auto device = ospGetCurrentDevice();
-  if (device == nullptr) {
-    std::cerr << "FATAL ERROR DURING GETTING CURRENT DEVICE!" << std::endl;
-    return 1;
-  }
+      auto device = ospGetCurrentDevice();
+      if (device == nullptr) {
+        std::cerr << "FATAL ERROR DURING GETTING CURRENT DEVICE!" << std::endl;
+        return 1;
+      }
 
-  ospDeviceSetString(device, "logOutput", "cout");
-  ospDeviceSetString(device, "errorOutput", "cerr");
+      ospDeviceSetString(device, "logOutput", "cout");
+      ospDeviceSetString(device, "errorOutput", "cerr");
 
-  ospDeviceCommit(device);
+      ospDeviceCommit(device);
 
-  // access/load symbols/sg::Nodes dynamically
-  loadLibrary("ospray_sg");
-  ospLoadModule("cpp");
+      // access/load symbols/sg::Nodes dynamically
+      loadLibrary("ospray_sg");
+      ospLoadModule("cpp");
 
-  parseExtraParametersFromComandLine(ac, av);
+      parseExtraParametersFromComandLine(ac, av);
 
-  ospray::imgui3D::init(&ac,av);
+      ospray::imgui3D::init(&ac,av);
 
-  // import the patches from the sample files (creates a default
-  // patch if no files were specified)
-  box3f worldBounds;
+      // import the patches from the sample files (creates a default
+      // patch if no files were specified)
+      box3f worldBounds;
 
-  auto renderer_ptr = sg::createNode("renderer", "Renderer");
-  auto &renderer = *renderer_ptr;
+      auto renderer_ptr = sg::createNode("renderer", "Renderer");
+      auto &renderer = *renderer_ptr;
 
-  auto &win_size = ospray::imgui3D::ImGui3DWidget::defaultInitSize;
-  renderer["frameBuffer"]["size"] = win_size;
-  renderer["rendererType"].setFlags(sg::NodeFlags::required |
-                                    sg::NodeFlags::gui_combo);
-  renderer["rendererType"] = std::string(g_prefix + "ao" + g_postfix);
+      auto &win_size = ospray::imgui3D::ImGui3DWidget::defaultInitSize;
+      renderer["frameBuffer"]["size"] = win_size;
+      renderer["rendererType"].setFlags(sg::NodeFlags::required |
+                                        sg::NodeFlags::gui_combo);
+      renderer["rendererType"] = std::string(g_prefix + "ao" + g_postfix);
 
-  auto &world = renderer["world"];
+      auto &world = renderer["world"];
 
-  for (auto file : files)
-    importOBJ_cpp(world.shared_from_this(), file);
+      for (auto file : files)
+        importOBJ_cpp(world.shared_from_this(), file);
 
-  addLightsToScene(renderer);
+      addLightsToScene(renderer);
 
-  // last, to be able to modify all created SG nodes
-  parseCommandLineSG(ac, av, renderer);
+      // last, to be able to modify all created SG nodes
+      parseCommandLineSG(ac, av, renderer);
 
-  ImGuiViewer window(renderer_ptr);
+      ImGuiViewer window(renderer_ptr);
 
-  auto &viewPort = window.viewPort;
-  // XXX SG is too restrictive: OSPRay cameras accept non-normalized directions
-  auto dir = normalize(viewPort.at - viewPort.from);
-  auto camera_ptr = sg::createNode("camera", "CppPerspectiveCamera");
-  renderer.add(camera_ptr);
-  auto &camera = *camera_ptr;
-  camera["dir"] = dir;
-  camera["pos"] = viewPort.from;
-  camera["up"]  = viewPort.up;
-  camera["fovy"] = viewPort.openingAngle;
-  camera["apertureRadius"] = viewPort.apertureRadius;
-  if (camera.hasChild("focusdistance"))
-    camera["focusdistance"] = length(viewPort.at - viewPort.from);
+      auto &viewPort = window.viewPort;
+      auto dir = normalize(viewPort.at - viewPort.from);
+      auto camera_ptr = sg::createNode("camera", "CppPerspectiveCamera");
+      renderer.add(camera_ptr);
+      auto &camera = *camera_ptr;
+      camera["dir"] = dir;
+      camera["pos"] = viewPort.from;
+      camera["up"]  = viewPort.up;
+      camera["fovy"] = viewPort.openingAngle;
+      camera["apertureRadius"] = viewPort.apertureRadius;
+      if (camera.hasChild("focusdistance"))
+        camera["focusdistance"] = length(viewPort.at - viewPort.from);
 
-  window.create("ospCppViewer: OSPRay C++-rendering Viewer");
+      window.create("ospCppViewer: OSPRay C++-rendering Viewer");
 
-  ospray::imgui3D::run();
-}
+      ospray::imgui3D::run();
+    }
 
   } // ::ospray::cpp_renderer
 } // ::ospray
